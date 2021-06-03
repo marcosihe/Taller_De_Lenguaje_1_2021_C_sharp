b85efdcb49eb6920ea109a664ba5141f6f0997b6
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-06-02 22:56:51 -0300

    Se cambio el nombre a la clase que contiene a las clases triangulo y rectangulo, y se hizo uso del constructor parametrizado para instanciar un objeto de tipo triangulo.

--- Program.cs
+++ Program.cs
@@ -9,15 +9,17 @@ namespace PrimerProyecto
         {
             Rectangulo rectangulo1 = new Rectangulo();
             Triangulo triangulo1 = new Triangulo();
+            Triangulo triangulo2 = new Triangulo(20,30);
 
             rectangulo1.Base = 15;
             rectangulo1.Altura = 10;
             triangulo1.BaseT = 15;
             triangulo1.AlturaT = 10;
 
-            Console.WriteLine("Perimetro del rectángulo: " + rectangulo1.perimetro());
-            Console.WriteLine("Área del rectángulo: " + rectangulo1.area());
-            Console.WriteLine("Área del triángulo: " + triangulo1.area());
+            Console.WriteLine("Perimetro del rectángulo 1: " + rectangulo1.perimetro());
+            Console.WriteLine("Área del rectángulo 1: " + rectangulo1.area());
+            Console.WriteLine("Área del triángulo 1: " + triangulo1.area());
+            Console.WriteLine("Área del triángulo 2: " + triangulo2.area());
         }
     }
 }
