4eadf4cf57956577b928d748563066c6e4b8a301
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-06-02 22:43:30 -0300

    Se agrego la clase Triangulo dentro de la clase FigurasGeometricas.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace PrimerProyecto
         static void Main(string[] args)
         {
             Rectangulo rectangulo1 = new Rectangulo();
+            Triangulo triangulo1 = new Triangulo();
+
             rectangulo1.Base = 15;
             rectangulo1.Altura = 10;
-            Console.WriteLine("Perimetro: " + rectangulo1.perimetro(rectangulo1.Base, rectangulo1.Altura));
+            triangulo1.BaseT = 15;
+            triangulo1.AlturaT = 10;
+
+            Console.WriteLine("Perimetro del rectángulo: " + rectangulo1.perimetro());
+            Console.WriteLine("Área del rectángulo: " + rectangulo1.area());
+            Console.WriteLine("Área del triángulo: " + triangulo1.area());
         }
     }
 }
--- Rectangulo.cs
+++ Rectangulo.cs
@@ -18,11 +18,35 @@ namespace FormasGeometricas
             this.altura_ = altura_;
         }
 
-        public int area(int x, int y){
-            return x + y;
+        public int area(){
+            return base_ * altura_;
         }
-        public int perimetro(int x, int y){
-            return 2*x + 2*y;
+        public int perimetro(){
+            return 2*base_ + 2*altura_;
         }
+    }
+
+    public class Triangulo
+    {
+        private int baseT;
+        private int alturaT;
+
+        public int BaseT { get => baseT; set => baseT = value; }
+        public int AlturaT { get => alturaT; set => alturaT = value; }
+
+        public Triangulo(){
+            BaseT = 0;
+            AlturaT = 0;
+        }
+
+        public Triangulo(int baseT, int alturaT){
+            this.baseT = baseT;
+            this.alturaT = alturaT;
+        }
+
+        public int area(){
+            return baseT * alturaT / 2;
+        }
+
     }
 }
